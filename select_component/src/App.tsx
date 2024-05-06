67e4b64b6f581643cacaa7ea8b419db31b5bcf88
rajat <vrajatlink@gmail.com>
2024-05-06 23:59:29 +0530

    ...

---
+++ select_component/src/App.tsx
@@ -0,0 +1,22 @@
+import { Select } from "./select"
+
+const options = [
+  {label:"first", value:1},
+  {label:"Second", value:2},
+  {label:"Third", value:3},
+  {label:"Fourth", value:4},
+  {label:"Fifth", value:5},  
+  
+
+
+]
+
+function App() {
+  return (
+    <>
+    <Select options={options}/>
+    </>
+  )
+}
+
+export default App
